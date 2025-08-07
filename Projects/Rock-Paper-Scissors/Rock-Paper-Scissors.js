7b481ba1974301eeb07e76e1d8f5306a0a090cb1
pedram-farrokhi <pedram.farrokhi.me@gmail.com>
2025-08-08 03:23:55 +0330

    Update Rock-Paper-Scissors.js

--- Projects/Rock-Paper-Scissors/Rock-Paper-Scissors.js
+++ Projects/Rock-Paper-Scissors/Rock-Paper-Scissors.js
@@ -1,1 +1,8 @@
+const gameContainer = document.querySelector(".container"),
+  userResult = document.querySelector(".user_result img"),
+  CPUResult = document.querySelector(".CPU_result img"),
+  result = document.querySelector(".result"),
+  optionImages = document.querySelectorAll(".option_images");
+
+console.log(gameContainer, userResult, CPUResult, result, optionImages);
 
